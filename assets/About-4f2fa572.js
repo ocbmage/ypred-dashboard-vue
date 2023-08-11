397308a228833b27ef34b696a4d1a60ea9a0b064
ocbmage <ocbmage@outlook.com>
2023-08-11 01:40:46 -0400

    fix stylesheet for dashboard

---
+++ assets/About-4f2fa572.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-32cde5fb.js";const r={name:"About",comments:{}};var s=function(){var n=this;return n._self._c,n._m(0)},_=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("About page")])])}],o=e(r,s,_,!1,null,null,null,null);const c=o.exports;export{c as default};
--- assets/About-a69ef2ad.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-371383e7.js";const r={name:"About",comments:{}};var s=function(){var n=this;return n._self._c,n._m(0)},_=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("About page")])])}],o=e(r,s,_,!1,null,null,null,null);const c=o.exports;export{c as default};
